a8295cb64cf0c9618acf1e8732eca4c34741ae7e
lalala-233 <2317987274@qq.com>
2023-06-09 23:45:46 +0800

    feat: 将程序中输入文件名改为在参数中指定

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-use std::{
-    error::Error,
-    fs::{self, File},
-    io::{self, Write, stdin},
-};
+use std::{cmp::Ordering, env, error::Error, fs};
 
 fn main() {
     run().unwrap_or_else(|error| {
@@ -11,27 +7,26 @@ fn main() {
 }
 
 fn run() -> Result<(), Box<dyn Error>> {
-    let mut path = String::new();
-    stdin().read_line(&mut path)?;
-    let path = path.trim();
-
-    // let path = "test.toml".to_string();
-    let content = fs::read_to_string(path)?;
-
-    let mut toml: anki_generate::Config = toml::from_str(&content)?;
-    for i in toml.generate_with_line()? {
-        println!("{i}");
-    }
-    // Read the origin.toml
-    // parse the header and the content
-    // Write the result.txt
-
+    let lines = get_lines()?;
+    println!("{lines}");
     Ok(())
 }
-fn write(content: Vec<String>) -> io::Result<()> {
-    let mut file = File::create("result.txt")?;
-    for line in content {
-        file.write_all(line.as_bytes())?;
-    }
-    Ok(())
+
+fn get_lines() -> Result<String, Box<dyn Error>> {
+    match env::args().len().cmp(&2) {
+        Ordering::Less => return Err("参数过少！".into()),
+        Ordering::Greater => return Err("参数过多！".into()),
+        _ => (),
+    };
+    let mut args = env::args();
+    args.next();
+    let path = args.next().unwrap();
+    let content = fs::read_to_string(path)?;
+    let mut toml: anki_generate::Config = toml::from_str(&content)?;
+    let lines = toml
+        .generate_with_line()?
+        .into_iter()
+        .map(|line| format!("{line}\n"))
+        .collect();
+    Ok(lines)
 }
