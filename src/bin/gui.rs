48addedb0a577877182d016d3b161dda15d67ff2
lalala-233 <2317987274@qq.com>
2023-10-07 19:01:50 +0800

    chore: gui 改为条件编译

--- src/bin/gui.rs
+++ src/bin/gui.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-
+#[cfg(feature = "gui")]
 fn main() -> Result<(), Box<dyn Error>> {
     anki_maker::app::app("Anki Maker")?;
     Ok(())
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#[cfg(feature="gui")]
 pub mod app;
 pub mod cli;
 mod emphasis_template;
